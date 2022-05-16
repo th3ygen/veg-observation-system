e48ed4f6188c49ba00d81bd357cc85a465380ceb
zaty <zatyaty5258@gmail.com>
2022-05-17 02:49:41 +0800

    sidebar update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
-
+import Sidebar from './components/Sidebar';
+import Topbar from './components/Topbar';
 function App() {
   return (
     <div className="App">
-      
+      <Sidebar></Sidebar>
+      <Topbar></Topbar>
     </div>
   );
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,43 @@
+import styles from "./Sidebar.module.scss"
+
+
 export default function Sidebar() {
     return <>
-        
+        <div className={styles.sidebar}>
+            <div className={styles.logoCard}>
+                <div className={styles.logo}>
+
+                </div>
+                <div className={styles.logoName}>
+                    JustGood
+                </div>
+            </div>
+            <div className={styles.navlist}>
+                <div className={`${styles.sideContent} ${styles.active}`}>
+                    <div className={styles.sideIcon}>
+
+                    </div>
+                    <div className={styles.sideName}>
+                        Plant report
+                    </div>
+                </div>
+                <div className={styles.sideContent}>
+                    <div className={styles.sideIcon}>
+
+                    </div>
+                    <div className={styles.sideName}>
+                        Growth Observation
+                    </div>
+                </div>
+                <div className={styles.sideContent}>
+                    <div className={styles.sideIcon}>
+
+                    </div>
+                    <div className={styles.sideName}>
+                        Crops Estimation
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 }
